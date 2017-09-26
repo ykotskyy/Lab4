68bdf6db08382a9cb0ff8f24dde18ff9b27bc0e6
Yuriy <Yuriy@LAPTOP-RKBD1B42>
2017-09-26 14:40:03 -0500

    cleaned up code

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,38 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+
+/**
+    *  this is a java doc about the code below.
+    */
+
+public class ReclamationProject {
+
+    /**
+     *
+     * @param a string
+     * @param b string
+     * @return returns a string
+     */
+        static String doit(final String a,  final String b) {
+        if (a.length() > b.length()) {
+            String c = "";
+            String wordone = a; // set c to a
+            String wordtwo = b;
+            wordtwo = c;
+            }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+
+                            if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                                 r = a.substring(i, i + j);
+                            }
+                        }
+                    }
         } return r; }
 }
